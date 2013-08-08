ec56d3a7abeb3fdd44b01c8341aacd18d361fc97
Q-Team <maltamira@gmail.com>
2013-08-08 13:17:05 -0300

    Web Service Note Methods

--- api.net/CPB.Backend.Web/API.NETWebService.asmx.cs
+++ api.net/CPB.Backend.Web/API.NETWebService.asmx.cs
@@ -25,7 +25,7 @@ namespace CPB.Backend.Web
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public User CreateUser(User user)
         {
-            User result = new User();
+            User result = null;
             using (UserManager manager = new UserManager())
             {
                 result = manager.Create(user);
@@ -37,7 +37,7 @@ namespace CPB.Backend.Web
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public User UpdateUser(User user)
         {
-            User result = new User();
+            User result = null;
             using (UserManager manager = new UserManager())
             {
                 result = manager.Update(user);
@@ -49,7 +49,7 @@ namespace CPB.Backend.Web
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public User GetUserByName(string userName)
         {
-            User result = new User();
+            User result = null;
             using (UserManager manager = new UserManager())
             {
                 result = manager.GetUserByName(userName);
@@ -61,7 +61,7 @@ namespace CPB.Backend.Web
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public User GetUserById(int userId)
         {
-            User result = new User();
+            User result = null;
             using (UserManager manager = new UserManager())
             {
                 result = manager.GetById(userId);
@@ -73,7 +73,7 @@ namespace CPB.Backend.Web
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public User ValidateUser(string userName, string password)
         {
-            User result = new User();
+            User result = null;
             using (UserManager manager = new UserManager())
             {
                 result = manager.ValidateUser(userName, password);
@@ -82,5 +82,60 @@ namespace CPB.Backend.Web
         }
 
         #endregion -- User Methods --
+
+        #region -- Note Methods --
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public Note CreateNote(Note note)
+        {
+            Note result = null;
+            using (NoteManager manager = new NoteManager())
+            {
+                result = manager.Create(note);
+            }
+            return result;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public Note UpdateUser(int userId, Note note)
+        {
+            Note result = null;
+            using (NoteManager manager = new NoteManager())
+            {
+                if (manager.ValidateNoteOwner(userId, note.Id))
+                    result = manager.Update(note);
+            }
+            return result;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public Note GetNoteById(int userId, int noteId)
+        {
+            Note result = null;
+            using (NoteManager manager = new NoteManager())
+            {
+                if (manager.ValidateNoteOwner(userId, noteId))
+                    result = manager.GetById(noteId);
+            }
+            return result;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public bool DeleteNote(int userId, Note note)
+        {
+            bool result = false;
+            using (NoteManager manager = new NoteManager())
+            {
+                if (manager.ValidateNoteOwner(userId, note.Id))
+                    result = manager.Delete(note.Id);
+            }
+            return result;
+        }
+
+        #endregion -- Note Methods --
     }
 }
